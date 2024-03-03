d68d8ce7f49d34a278318d7d8f4ef5729ccc5083
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-03 15:26:26 +0300

    Added project TypesAndVariables

---
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
