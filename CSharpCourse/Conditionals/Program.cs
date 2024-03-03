5b330739ac9bcd3ac27aae70742f7e5305ea85d4
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-03 16:59:33 +0300

    Added if-else

---
+++ CSharpCourse/Conditionals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var number = 1;
+
+            if (number == 0)
+            {
+                Console.WriteLine("Number is 0");
+            }
+            else
+            {
+                Console.WriteLine("Number is not 0");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
