deefbe34f0dc2453ac305ce8869839087fa8d55b
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-03 15:49:20 +0300

    Added byte value type

--- CSharpCourse/TypesAndVariables/Program.cs
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -12,11 +12,12 @@ namespace TypesAndVariables
         private static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
+            byte numberByte = 255;
             short numberShort = 32767;  // 16 bit
             int numberInt = 2147483647;   // 32 bit
             long numberLong = -214783648;   // 64 bit
 
-            Console.WriteLine("Numbers are: {0} | {1} | {2}", numberShort, numberInt, numberLong);
+            Console.WriteLine("Numbers are: {0} | {1} | {2} | {3}", numberByte, numberShort, numberInt, numberLong);
             Console.ReadLine();
         }
     }
