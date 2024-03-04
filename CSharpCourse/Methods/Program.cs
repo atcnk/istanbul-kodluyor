046dde61aa8f400c8e86171fb3e7b8e0dce57646
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-04 17:45:24 +0300

    Added methods with parameters

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -14,13 +14,19 @@ namespace Methods
             Add();
             Add();
             Add();
-
+            var result = Add2(20, 30);
+            Console.WriteLine("result: {0}", result);
             Console.ReadLine();
         }
 
         static void Add()
         {
             Console.WriteLine("Added");
+        }
+
+        static int Add2(int number1, int number2)
+        {
+            return number1 + number2;
         }
     }
 }
