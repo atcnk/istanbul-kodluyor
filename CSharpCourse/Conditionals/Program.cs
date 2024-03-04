afaa8ceb29ea7f2f2cc27827c30da3382abf5c59
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-04 17:35:03 +0300

    Added or - and conditions

--- CSharpCourse/Conditionals/Program.cs
+++ CSharpCourse/Conditionals/Program.cs
@@ -25,20 +25,33 @@ namespace Conditionals
             //    Console.WriteLine("Number is not 0 or 1");
             //}
 
-            switch (number)
-            {
-                case 0:
-                    Console.WriteLine("Number is 0");
-                    break;
-                case 1:
-                    Console.WriteLine("Number is 1");
-                    break;
-                default:
-                    Console.WriteLine("Number is not 0 or 1");
-                    break;
-            }
+            //switch (number)
+            //{
+            //    case 0:
+            //        Console.WriteLine("Number is 0");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("Number is 1");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Number is not 0 or 1");
+            //        break;
+            //}
 
             //Console.WriteLine(number == 0 ? "Number is 0" : "Number is not 0");
+
+            if (number >= 0 && number <= 100)
+            {
+                Console.WriteLine("Number is between 0-100");
+            }
+            else if (number > 100 && number <= 200)
+            {
+                Console.WriteLine("Number is between 101-200");
+            }
+            else if (number > 200 || number < 0)
+            {
+                Console.WriteLine();
+            }
 
             Console.ReadLine();
         }
