bb8eb65d808d3487dafdeeff50f2ce3110ca95af
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-10 20:53:10 +0300

    Added ref keyword

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -15,7 +15,14 @@ namespace Methods
             Add();
             Add();
             var result = Add2();
-            Console.WriteLine("result: {0}", result);
+
+            int number1 = 20;
+            int number2 = 100;
+
+            var result2 = Add3(ref number1, number2);
+
+            Console.WriteLine("result: {0}", result2);
+            Console.WriteLine("number1: {0}", number1);
             Console.ReadLine();
         }
 
@@ -27,6 +34,12 @@ namespace Methods
         static int Add2(int number1 = 20, int number2 = 30)
         {
             return number1 + number2;
+        }
+
+        static int Add3(ref int number1, int number2)
+        {
+            number1 = 30;
+            return (number1 + number2);
         }
     }
 }
