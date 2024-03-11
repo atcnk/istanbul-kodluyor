765d2a6ab5186ff0a69956ea64c3acf600486c0c
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-11 09:51:20 +0300

    Added loops project with for loop

---
+++ Courses/CSharpCourse/Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i >= 0; i-=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Finished!");
+
+            Console.ReadLine();
+        }
+    }
+}
