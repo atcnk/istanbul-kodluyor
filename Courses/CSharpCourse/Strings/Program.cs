5f39451dbcc7d0c429e3ba0f6420c51688dd9bb1
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-11 10:12:13 +0300

    Updated strings program with string methods

--- Courses/CSharpCourse/Strings/Program.cs
+++ Courses/CSharpCourse/Strings/Program.cs
@@ -10,6 +10,28 @@ namespace Strings
     {
         static void Main(string[] args)
         {
+            //Intro();
+
+            string sentence = "My name is atacan";
+            var result = sentence.Length;
+            var result2 = sentence.Clone();
+            sentence = "My name is kilic";
+            var result3 = sentence.EndsWith("n");
+            var result4 = sentence.StartsWith("a");
+            var result5 = sentence.IndexOf("name");
+            var result6 = sentence.IndexOf(" ");
+            var result7 = sentence.LastIndexOf(" ");
+            var result8 = sentence.Substring(3, 4);
+            var result9 = sentence.Insert(0, "HELLO");
+            var result10 = sentence.Replace(" ", "_");
+            var result11 = sentence.Remove(2, 6);
+
+            Console.WriteLine(result11);
+            Console.ReadLine();
+        }
+
+        private static void Intro()
+        {
             string city = "Ankara";
             //Console.WriteLine(city[0]);
 
@@ -21,7 +43,6 @@ namespace Strings
             string city2 = "Istanbul";
 
             Console.WriteLine(String.Format($"{city} {city2}"));
-            Console.ReadLine();
         }
     }
 }
