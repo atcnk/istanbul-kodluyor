a2be99dd0552d8543be078a47b96246eec17d948
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-04-09 18:54:16 +0300

    Added product feature

--- LiveSessions/Tobeto.NET4A/Business/Features/Products/Quaries/GetById/GetByIdProductResponse.cs
+++ LiveSessions/Tobeto.NET4A/Business/Features/Products/Quaries/GetById/GetByIdProductResponse.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Business.Features.Products.Dtos
+namespace Business.Features.Products.Queries.GetById
 {
-    public class ListProductResponse
+    public class GetByIdProductResponse
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public double UnitPrice { get; set; }
+        public int Stock { get; set; }
     }
 }
