ad7c6b60c6823ebfb18f4add0863f37d41a00837
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-04-12 22:22:18 +0300

    Added: InterfaceAndAbstractDemo

---
+++ Courses/CSharpCourse/InterfaceAndAbstractDemo/Concrete/NeroCustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using InterfaceAndAbstractDemo.Abstract;
+using InterfaceAndAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAndAbstractDemo.Concrete
+{
+    public class NeroCustomerManager : BaseCustomerManager
+    {
+        private ICustomerCheckService _customerCheckService;
+
+        public NeroCustomerManager(ICustomerCheckService customerCheckService)
+        {
+            _customerCheckService = customerCheckService;
+        }
+
+        public override void Save(Customer customer)
+        {
+            if (_customerCheckService.CheckIfRealPerson(customer))
+            {
+                base.Save(customer);
+            }
+            else
+            {
+                throw new Exception("Not a valid person");
+            }
+        }
+    }
+}
---
+++ Courses/CSharpCourse/InterfaceAndAbstractDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAndAbstractDemo.Abstract;
+using InterfaceAndAbstractDemo.Concrete;
+using InterfaceAndAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAndAbstractDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new CustomerCheckManager());
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(1985, 1, 6), FirstName = "Engin", LastName = "Demiroğ", NationalityId = "" });
+            Console.ReadLine();
+        }
+    }
+}
