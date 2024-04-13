7d3f7e0b465dd1d7d68f7d2d2c07ef5db970b945
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-04-13 17:14:09 +0300

    Project InterfacesDemo2 added.

---
+++ Courses/CSharpCourse/InterfacesDemo2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager();
+            customerManager.Add();
+
+            IPersonManager employeeManager = new EmployeeManager();
+            employeeManager.Add();
+        }
+    }
+
+    interface IPersonManager
+    {
+        void Add();
+        void Update();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer added");
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Employee added.");
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add()
+        {
+           
+        }
+    }
+}
