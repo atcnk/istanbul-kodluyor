7e30110f27bc70266fd5195ab40a285aa3117a8e
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-04-13 16:40:36 +0300

    ReferenceTypes project added

---
+++ Courses/CSharpCourse/ReferenceTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int, decimal, float, enum bool value type
+            int number1 = 10;
+            int number2 = 20;
+
+            number1 = number2;
+
+            number2 = 100;
+
+            Console.WriteLine(number1);
+
+            Console.ReadLine();
+        }
+    }
+}
