0af5f634170a4ea156dcd2791f3b372144f38c85
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-04-13 17:05:43 +0300

    Project Constructors added.

---
+++ Courses/CSharpCourse/Constructors/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer {  Id = 1, FirstName = "Engin", LastName = "Demirog", City = "Ankara"};
+            Customer customer2 = new Customer(2, "Derin", "Demirog", "Ankara");
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+        }
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+            
+        }
+
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public string City { get; set; }
+    }
+}
